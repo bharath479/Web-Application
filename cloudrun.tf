7c846173b2f18482168b8c3d025660cec79539c6
Bharath-333-scm <dadibharath30@gmail.com>
2026-02-16 11:00:58 +0530

    cloudrun.tf was updated

--- cloudrun.tf
+++ cloudrun.tf
@@ -43,7 +43,7 @@ resource "google_cloud_run_v2_service" "app" {
   }
 
   depends_on = [
-    google_project_service.run_api,
+    google_project_service.cloudrun_api,
     google_project_service.artifact_api
   ]
 }
